31339254b83344772bce2f9dbae93e5c04ebfc41
tomasz-wozniak-apzumi <tomasz.wozniak@apzumi.pl>
2026-02-27 09:26:39 +0100

    chore: apply latest screenshot changes to layout including specific speed angle updates

--- main.js
+++ main.js
@@ -20,18 +20,18 @@ let tinnitusActive = false;
 const speed = 15;
 
 const guiSettings = {
-    carX: 1.4, carY: -1.56, carZ: -3.28, carScale: 0.03,
-    carRotX: 1.498407, carRotY: 3.141592, carRotZ: -0.00159,
+    carX: 1.4, carY: -1, carZ: -3.28, carScale: 0.03,
+    carRotX: 1.598407, carRotY: 3.141592, carRotZ: -0.00159,
     passX: -0.16, passY: -0.78, passZ: 0.94, passScale: 1.15,
     passRotX: 0, passRotY: 3.141592, passRotZ: 0,
-    roadX: -10.7, roadY: -3.2, roadZ: 0, roadScale: 3,
-    roadRotX: 0.128407, roadRotY: 1.608407, roadRotZ: 0,
+    roadX: -16.2, roadY: 2, roadZ: 0, roadScale: 3,
+    roadRotX: -0.02159, roadRotY: 1.598407, roadRotZ: 0,
     userX: -1, userY: -0.23, userZ: 0.82,
     showUserSpawn: false, // Turned off by default so the red sphere does not obstruct patient view
     showLabels: false,
     incStartX: 20, incStartY: -1.5, incStartZ: -60, incRotY: -0.785, incScale: 0.03,
-    incStartTime: 10.0, incSpeedX: 20, incSpeedZ: 25, crashThresholdX: 1.0,
-    playScenario: false, // Started paused for safety as requested by "Play / Pause" unchecked
+    incStartTime: 10.0, incSpeedX: -17.5, incSpeedZ: 25, crashThresholdX: 1.0,
+    playScenario: true,
     scenarioSpeed: 1.0,
     resetScenario: () => {
         scenarioState = 'NEUTRAL';
